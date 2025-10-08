c815f36c569bf86a97dfe703b552301c9b3d395d
Alex Kremiansky <alextesy@gmail.com>
2025-10-08 21:08:13 +0300

    wip

--- tests/test_reddit_scraper_new.py
+++ tests/test_reddit_scraper_new.py
@@ -5,7 +5,7 @@ from unittest.mock import Mock, patch
 
 import pytest
 
-from ingest.reddit_scraper import RateLimiter, RedditScraper, ScrapeStats
+from jobs.ingest.reddit_scraper import RateLimiter, RedditScraper, ScrapeStats
 
 
 class TestRateLimiter:
